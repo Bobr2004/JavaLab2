5bbbfac31985ffefe2bf9b372314f813c2dce0d8
bohdan <shovkopliasbohdan@gmail.com>
2024-11-10 10:45:26 +0200

    XSLT Transformer build file include

--- src/main/java/com/labab/XSLTTransformer.java
+++ src/main/java/com/labab/XSLTTransformer.java
@@ -8,18 +8,15 @@ import java.io.File;
 public class XSLTTransformer {
     public static void main(String[] args) {
         try {
-            // Load the XML input file
             File xmlFile = new File("src/main/resources/beer.xml");
-            File xsltFile = new File("src/main/resources/beer_transform.xslt"); // Use .xsl for XSLT file
-            File outputFile = new File("src/main/resources/transformed_beer.xml"); // Specify output file name
+            File xsltFile = new File("src/main/resources/beer_transform.xslt"); 
+            File outputFile = new File("src/main/resources/transformed_beer.xml"); 
 
-            // Set up the transformer
             TransformerFactory factory = TransformerFactory.newInstance();
             Source xslt = new StreamSource(xsltFile);
             Transformer transformer = factory.newTransformer(xslt);
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty("{http://xml.apache.org/xalan}indent-amount", "4"); // Specify indentation size
-            // Perform the transformation
+            transformer.setOutputProperty("{http://xml.apache.org/xalan}indent-amount", "4"); 
             Source xmlSource = new StreamSource(xmlFile);
             Result output = new StreamResult(outputFile);
             transformer.transform(xmlSource, output);
